95394f329a6241ea50ee6d71602840cd871a39c3
HaiderAli228 <flutter2830@gmail.com>
2024-10-21 17:29:14 +0500

    Remove API key

--- lib/view-model/app_links.dart
+++ lib/view-model/app_links.dart
@@ -1,3 +1,3 @@
 class AppLinks{
-  static const String chatbotPostApi = "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" ;
+  static const String chatbotPostApi = "" ;
 }
