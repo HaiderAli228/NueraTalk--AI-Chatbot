3cd913357a478c279ef2908c123f03e4ba7fe1c6
HaiderAli228 <flutter2830@gmail.com>
2024-10-24 18:20:11 +0500

    adding new theme color

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColor{
-  static const Color themeColor = Colors.black;
+  static const Color themeColor = Colors.blue;
   static const Color themeTextColor = Colors.white;
   static const Color bodyBGColor = Colors.white;
 }
